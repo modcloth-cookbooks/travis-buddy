4ab3601c58b1c3c188b3555078e25e045a56be8f
Rafe Colton <r.colton@modcloth.com>
2013-07-31 21:20:09 -0400

    Also installing aws, since it is a dependency of `download-artifact`

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,6 +1,9 @@
 remote_file "#{node['install_prefix']}/bin/tb" do
   source "https://raw.github.com/modcloth-labs/travis-buddy/#{node['travis_buddy']['rev']}/tb"
-  mode 0555
+  mode 0755
+end
 
-  action :create
+remote_file "#{node['install_prefix']}/bin/aws" do
+  source 'https://raw.github.com/timkay/aws/master/aws'
+  mode 0755
 end
