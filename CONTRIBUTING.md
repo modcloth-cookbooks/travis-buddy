89356c39b26b0dbd93b3f14ff5cf1942b8e5532b
Dan Buch <d.buch@modcloth.com>
2013-08-02 22:48:34 -0400

    I prefer this lamp on this table

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,12 +1,9 @@
 # Contributing
 
-## To Contribute...
-
 1. Fork the repository on GitHub
-2. Created a named feature branch (e.g. `add_component_x`)
-3. Make your changes to your feature branch
-4. Write tests for your change (if applicable)
-5. Run the tests, ensuring that they all pass
-6. Submit a Pull Request using GitHub
-7. Don't forget to add yourself to the AUTHORS file - alphabetically by
+1. Created a named feature branch (e.g. `add_component_x`)
+1. Make your changes to your feature branch
+1. Write tests for your change (if applicable)
+1. Submit a Pull Request using GitHub
+1. Don't forget to add yourself to the `AUTHORS.md` file - alphabetically by
    first name please
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,9 +1,38 @@
-remote_file "#{node['install_prefix']}/bin/tb" do
-  source "https://raw.github.com/modcloth-labs/travis-buddy/#{node['travis_buddy']['rev']}/tb"
-  mode 0755
-end
+#
+# Cookbook Name:: travis-buddy
+# Recipe:: install
+#
+# Copyright 2013, ModCloth, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
 
-remote_file "#{node['install_prefix']}/bin/aws" do
-  source 'https://raw.github.com/timkay/aws/master/aws'
-  mode 0755
+{
+  'tb' => "https://raw.github.com/modcloth-labs/travis-buddy/#{node['travis_buddy']['rev']}/tb",
+  'aws' => 'https://raw.github.com/timkay/aws/master/aws'
+}.each do |exe,source_url|
+  remote_file "#{node['install_prefix']}/bin/#{exe}" do
+    source source_url
+    mode 0755
+    owner 'root'
+    group 'root'
+    action node['travis_buddy']['install_action']
+  end
 end
